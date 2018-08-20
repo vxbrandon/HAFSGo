19951700a0f303b5da38e866623bcb08f97d98a9
lukeleeai <disrupt17@gmail.com>
2018-08-20 09:30:23 +0900

    first commit

---
+++ agent_network.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import numpy as np
+from cocob_optimizer import COCOB
+
+class Agent:
+
+    def __init__(self):
+        pass
+
+    def act(self):
+    	pass 
+
+    def predict(self):
+    	pass
