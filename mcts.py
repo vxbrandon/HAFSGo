a2ee16a7b9df794b7af35d4c7ae03986f05bc881
lukeleeai <disrupt17@gmail.com>
2018-08-21 14:55:15 +0900

    updated mcts file

--- mcts.py
+++ mcts.py
@@ -1,12 +1,54 @@
-from agent_network import Agent as agent
-import numpy as np
+# from agent_network import Agent as agent
+from gym import go  # go environment 
+# import numpy as np
+
 
 class MCTS:
 
 	def __init__(self):
-		self.state = None
+		self.env = go.reset()
+		self.nodes = np.array([])  # saved nodes
 
-	def select():
-		agent.get_policy()
+	def select(self, state):
+		action = agent.act(state)
+		policy = agent.get_policy(state)
+		next_state, reward, done = go.step(action)
+		
+		node = Node(next_state)
+		if node.state not in [n.state for n in self.nodes]:
+			node.policy = policy
+			node.n += 1
+			self.nodes.append(node)
+		else:
 
-	def 
+
+		return next_state
+
+	def simulate(self):
+		# expand and evaluate
+		pass
+	
+	def backup(self):
+		pass		
+
+
+class Node():
+
+	def __init__(self, state):
+		self.state = state
+		self.policy = None
+		self.value = None
+		self.n = 0
+		self.q = 0
+		self.w = 0
+
+
+
+
+
+
+
+
+
+
+
