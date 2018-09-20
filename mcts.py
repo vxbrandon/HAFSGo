03a7f868dad4373b27b226ea84d10a83dead5054
vxbrandon <vxbrandon@gmail.com>
2018-09-20 14:14:59 +0900

    Update mcts.py

--- mcts.py
+++ mcts.py
@@ -40,7 +40,71 @@ class Node():
 		self.n = 0
 		self.q = 0
 		self.w = 0
+		
+# 위에거는 그냥 나둘게
 
+import numpy as np
+import math
+
+
+class Mcts:
+    def __init__(self, model, history, c_puct, temperature):
+        self.model = model
+        self.history = history
+        self.c_puct = c_puct
+        self.temperature = temperature
+
+    def U(num):
+        pass
+
+    def Q(num):
+        pass
+
+
+    def search(s, env):
+
+        s = tuple(s)
+
+        if env.ended():
+            return env.reward()
+
+        if s not in history:
+            p, v = self.model(s)
+            action_lst = env.action_lst() # including pass
+            node_lst = np.array([node(s, action, 0, 0, 0, p) for action in action_lst])
+            self.history[s] = node_lst
+            return v
+
+        a, max_U = -1, -float('inf')
+        node_lst = history[s]
+        sigma_n = sum(n for n in node_lst)
+        node_update = 0
+
+        for node in node_lst:
+            U = node.q + self.c_puct * node.p * math.sqrt(sigma_n) / node.n
+            if U > max_U:
+                node_update = node
+                a = node.a
+                max_U = U
+                
+                
+        env.move(a) # a = -1 --> pass
+        next_s = env.state()
+
+        v = search(next_s, env)
+        node_update.n += 1
+        node_update.w += v
+        node_update.Q += node_update.w/node.update.n
+
+        return v
+    
+    def simulation(self, state):
+        #multi processing
+        return self.next_move(state)
+        
+
+    def next_move(self, state):
+        pass
 
 
 
